a562f21dbb8917239063f2eeba54f0baf0696e48
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-12 12:40:52 +0300

    Suorita tehtävä 3.13 puhelintiedot tietokannasta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -10,31 +11,10 @@ app.use(cors())
 morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456 ",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -101,7 +81,7 @@ app.post('/api/persons', (request, response) => {
     }
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })

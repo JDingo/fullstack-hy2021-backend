49faa8f22ab39af8e8d514bcf1c7deee657850e0
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-12 14:22:49 +0300

    Suorita tehtävä 3.18 yksittäisen henkilön tiedot ja info käsittely

--- index.js
+++ index.js
@@ -19,21 +19,24 @@ app.get('/api/persons', (request, response) => {
     .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-    response
-        .send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
-        .catch(error => next(error))
+app.get('/info', (request, response, next) => {
+    Person.find({}).then(result => {
+        response
+            .send(`<p>Phonebook has info for ${result.length} people</p> <p>${new Date()}</p>`)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
